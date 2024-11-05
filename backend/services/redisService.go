e09d2e2e69033e8e089534f933397488818b84a8
AshwinJharia <ashwinjharia48@gmail.com>
2024-11-05 10:18:15 +0530

    update info for backend hosting

--- backend/services/redisService.go
+++ backend/services/redisService.go
@@ -2,17 +2,30 @@ package services
 
 import (
 	"context"
+	"os"
 	"strconv"
 
 	"github.com/go-redis/redis/v8"
 )
 
 var ctx = context.Background()
-var rdb = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "",
-	DB:       0,
-})
+
+//	var rdb = redis.NewClient(&redis.Options{
+//		Addr:     "localhost:6379",
+//		Password: "",
+//		DB:       0,
+//	})
+func NewRedisClient() *redis.Client {
+	redisURL := os.Getenv("REDIS_URL")
+	options, err := redis.ParseURL(redisURL)
+	if err != nil {
+		panic(err)
+	}
+
+	return redis.NewClient(options)
+}
+
+var rdb = NewRedisClient()
 
 func UpdateScore(username string, score int) {
 	rdb.HSet(ctx, "scores", username, score)
